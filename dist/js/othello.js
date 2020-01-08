01d607f12cecd6bc57d8675a7d54fce0d39be494
nakama <mingjiazhenyoutai@gmail.com>
2020-01-08 13:10:26 +0900

    元に戻した

--- dist/js/othello.js
+++ dist/js/othello.js
@@ -1,3 +1,5 @@
+/**/
+
 var row, yAxis, xAxis, player, myColor, opponentColor, pieces, hint;
 row = [];
 yAxis = 8;
@@ -6,7 +8,6 @@ player = 0; // 0:先攻(黒), 1:後攻(白)
 myColor = 0;
 opponentColor = 1;
 
-// 処理
 $(function() {
   initBoard();
   checkHint();
@@ -22,12 +23,10 @@ function initBoard() {
     row = $(`<div class="row y${y}"></div>`);
     for (var x = 0; x < xAxis; x++) {
       if ((x === 3 && y === 4) || (x === 4 && y === 3)) {
-        pieces =
-          '<div class="pieces" style="transform: rotateY(0deg);"><div class="pieces0"></div><div class="pieces1"></div></div>';
+        pieces = '<div class="pieces0"></div>';
         grid = $(`<div class="grid x${x}-y${y}">${pieces}</div></div>`);
       } else if ((x === 3 && y === 3) || (x === 4 && y === 4)) {
-        pieces =
-          '<div class="pieces" style="transform: rotateY(180deg);"><div class="pieces0"></div><div class="pieces1"></div></div>';
+        pieces = '<div class="pieces1"></div>';
         grid = $(`<div class="grid x${x}-y${y}">${pieces}</div></div>`);
       } else {
         pieces = '<div class="pieces"></div>';
@@ -67,7 +66,7 @@ function initInfo() {
 
 function checkHint() {
   // ヒントを表示させる場所を探す
-  searchHint([-1, 0]); // [y,x]
+  searchHint([-1, 0]);
   searchHint([1, 0]);
   searchHint([0, -1]);
   searchHint([0, 1]);
@@ -114,48 +113,6 @@ function searchHint(direction) {
   }
 }
 
-function boardCheck() {
-  var hintNum, hint0, hint1, notEmpty, black, white;
-  hintNum = 0;
-  hint0 = 0;
-  hint1 = 0;
-  notEmpty = 0;
-  black = 0;
-  white = 0;
-  for (var y = 0; y < yAxis; y++) {
-    for (var x = 0; x < xAxis; x++) {
-      if ($(`.x${x}-y${y}`)[0].innerHTML === '<div class="hint0"></div>')
-        hint0++;
-      if ($(`.x${x}-y${y}`)[0].innerHTML === '<div class="hint1"></div>')
-        hint1++;
-      if (row[y][x].contents === 0 || row[y][x].contents === 1) notEmpty++;
-      if (row[y][x].contents === 0) black++;
-      if (row[y][x].contents === 1) white++;
-    }
-  }
-  // $(".player0")[0].children[1].innerHTML = "×" + black;
-  // $(".player1")[0].children[1].innerHTML = "×" + white;
-  if ((!hint0 && player === 0) || (!hint1 && player === 1)) {
-    changePlayer();
-    checkHint();
-    for (var y = 0; y < yAxis; y++) {
-      for (var x = 0; x < xAxis; x++) {
-        if ($(`.x${x}-y${y}`)[0].innerHTML === hint) {
-          hintNum++;
-        }
-      }
-    }
-  }
-  if (
-    notEmpty === 64 ||
-    (!hint0 && !hint1 && notEmpty !== 64 && !hintNum) ||
-    !black ||
-    !white
-  )
-    // 対戦結果を表示
-    decideWinner();
-}
-
 function putPieces() {
   $(".grid").click(function() {
     for (var y = 0; y < yAxis; y++) {
@@ -174,20 +131,13 @@ function putPieces() {
           changePieces(search(x, y, [-1, 1]));
           changePieces(search(x, y, [1, -1]));
           changePieces(search(x, y, [1, 1]));
-
-          if (!player) {
-            this.innerHTML =
-              '<div class="pieces" style="transform: rotateY(0deg);"><div class="pieces0"></div><div class="pieces1"></div></div>';
-          } else {
-            this.innerHTML =
-              '<div class="pieces" style="transform: rotateY(180deg);"><div class="pieces0"></div><div class="pieces1"></div></div>';
-          }
+          this.innerHTML = `<div class="pieces${player}"></div>`;
           row[y][x].contents = player;
 
-          // プレイヤーの変更
+          // プレイヤーを変える
           changePlayer();
 
-          // ヒントの表示,リセット
+          // ヒントの表示をリセット
           resetHint();
           checkHint();
           // ボード内を監視
@@ -199,10 +149,9 @@ function putPieces() {
 }
 
 function search(x, y, direction) {
-  var chainY, chainX, change;
-  chainY = y + direction[0];
-  chainX = x + direction[1];
-  change = [];
+  var chainY = y + direction[0];
+  var chainX = x + direction[1];
+  var change = [];
   if (chainY >= 0 && chainY < 8 && chainX >= 0 && chainX < 8) {
     while (row[chainY][chainX].contents === opponentColor) {
       change.push(row[chainY][chainX]);
@@ -230,11 +179,7 @@ function changePieces(array) {
   if (array.length !== 0) {
     for (let i = 0; i < array.length; i++) {
       grid = $(`.${array[i].coordinates}`)[0];
-      if (!player) {
-        grid.children[0].style.transform = "rotateY(0deg)";
-      } else {
-        grid.children[0].style.transform = "rotateY(180deg)";
-      }
+      grid.innerHTML = `<div class="pieces${player}"></div>`;
       array[i].contents = player;
     }
   }
@@ -260,6 +205,46 @@ function resetHint() {
       }
     }
   }
+}
+
+function boardCheck() {
+  var hintNum, hint0, hint1, notEmpty, black, white;
+  hintNum = 0;
+  hint0 = 0;
+  hint1 = 0;
+  notEmpty = 0;
+  black = 0;
+  white = 0;
+  for (var y = 0; y < yAxis; y++) {
+    for (var x = 0; x < xAxis; x++) {
+      if ($(`.x${x}-y${y}`)[0].innerHTML === '<div class="hint0"></div>')
+        hint0++;
+      if ($(`.x${x}-y${y}`)[0].innerHTML === '<div class="hint1"></div>')
+        hint1++;
+      if (row[y][x].contents === 0 || row[y][x].contents === 1) notEmpty++;
+      if (row[y][x].contents === 0) black++;
+      if (row[y][x].contents === 1) white++;
+    }
+  }
+  if ((!hint0 && player === 0) || (!hint1 && player === 1)) {
+    changePlayer();
+    checkHint();
+    for (var y = 0; y < yAxis; y++) {
+      for (var x = 0; x < xAxis; x++) {
+        if ($(`.x${x}-y${y}`)[0].innerHTML === hint) {
+          hintNum++;
+        }
+      }
+    }
+  }
+  if (
+    notEmpty === 64 ||
+    (!hint0 && !hint1 && notEmpty !== 64 && !hintNum) ||
+    !black ||
+    !white
+  )
+    // 対戦結果を表示
+    decideWinner();
 }
 
 function decideWinner() {
