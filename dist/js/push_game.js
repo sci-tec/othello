98c02efb21e1718f28cd547ab37d00b57a8b58af
cyfase-g <cyfase@gmail.com>
2020-01-08 13:43:21 +0900

    pusherテストファイル

--- dist/js/push_game.js
+++ dist/js/push_game.js
@@ -1,1 +1,13 @@
 //hello
+ // Enable pusher logging - don't include this in production
+ Pusher.logToConsole = true;
+
+ var pusher = new Pusher('ac2b1faa8b9a8094de41', {
+   cluster: 'ap3',
+   forceTLS: true
+ });
+
+ var channel = pusher.subscribe('table001');
+ channel.bind('my-event', function(data) {
+   console.log(JSON.stringify(data));
+ });
