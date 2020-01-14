0894d6a36b2c125eef434b4d7f3dcfdcf4ec90e7
Hajime Nagata <hajime.nagata@ncreate.net>
2020-01-14 13:19:55 +0900

    index.html -> index.php

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,12 +1,11 @@
 $(function(){
     $("body").append('<div id="navi"></div>')
     $("#navi").append(
-        '<a href="index.html">TOP</a>',
+        '<a href="/">TOP</a>',
         // '<a href="match_2.html">MATCH</a>',
         // '<a href="roomsearch_3.html">ROOM SEARCH</a>',
         // '<a href="roommake_4.html">ROOM MAKE</a>',
-        '<a href="roomsearch.php">ROOM SEARCH</a>'
+        '<a href="roomsearch.php">ROOM SEARCH</a>',
         '<a href="game.php">GAME</a>'
     )
-
 });
