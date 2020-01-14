adebb791da855ebaa561d4dc915700d4c5518d1e
Hajime Nagata <hajime.nagata@ncreate.net>
2020-01-14 13:53:56 +0900

    app.js 更新

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,11 @@
-alert("hello world");
+$(function(){
+    $("body").append('<div id="navi"></div>')
+    $("#navi").append(
+        '<a href="/">TOP</a>',
+        // '<a href="match_2.html">MATCH</a>',
+        // '<a href="roomsearch_3.html">ROOM SEARCH</a>',
+        // '<a href="roommake_4.html">ROOM MAKE</a>',
+        '<a href="roomsearch.php">ROOM SEARCH</a>',
+        '<a href="game.php">GAME</a>'
+    )
+});
