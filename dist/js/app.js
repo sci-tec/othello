51e995716eb0f811adcec331f7f2ebd130010f03
Hajime Nagata <hajime.nagata@ncreate.net>
2020-01-21 11:31:58 +0900

    app.js更新 jsでgetパラメータを取得する関数追加

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,7 @@
 $(function(){
     $("body").append('<div id="navi"></div>')
     $("#navi").append(
-        '<a href="/">TOP</a>',
+        '<a href="index.php">TOP</a>',
         // '<a href="match_2.html">MATCH</a>',
         // '<a href="roomsearch_3.html">ROOM SEARCH</a>',
         // '<a href="roommake_4.html">ROOM MAKE</a>',
@@ -9,3 +9,16 @@ $(function(){
         '<a href="game.php">GAME</a>'
     )
 });
+
+var getUrlVars = function() {
+    var vars = {};
+    var param = location.search.substring(1).split("&");
+    for (var i = 0; i < param.length; i++) {
+      var keySearch = param[i].search(/=/);
+      var key = "";
+      if (keySearch != -1) key = param[i].slice(0, keySearch);
+      var val = param[i].slice(param[i].indexOf("=", 0) + 1);
+      if (key != "") vars[key] = decodeURI(val);
+    }
+    return vars;
+};
