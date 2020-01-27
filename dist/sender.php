5f93e4aef581dd5d40f630d5f6788157ec4cfec7
Hajime Nagata <hajime.nagata@ncreate.net>
2020-01-27 10:42:12 +0900

    docker関連削除  sender.js 追加　

--- dist/sender.php
+++ dist/sender.php
@@ -1,23 +1,41 @@
 <?php
-  require __DIR__ . '/vendor/autoload.php';
-  $options = array(
-    'cluster' => 'ap3',
-    'useTLS' => true
-  );
-  $pusher = new Pusher\Pusher(
-    'ac2b1faa8b9a8094de41',
-    'd078b9ab01a564abf58f',
-    '906718',
-    $options
-  );
-
   $newGET = [];
   foreach ($_GET as $key => $value) {
     $newGET[$key] = preg_replace('/[^0-9a-zA-Z]/', '', $value);
   }
 
-  $tableId = $newGET["tableId"];
-  $type = $newGET["type"];
+  //*/
+  sendByPHP($newGET); // mac
+  /*/
+  sendToNodeServer($newGET); // mac以外
+  //*/
 
-  $pusher->trigger($tableId, $type, $newGET);
+  function sendByPHP($newGETParams) {
+    require __DIR__ . '/vendor/autoload.php';
+    $options = array(
+      'cluster' => 'ap3',
+      'useTLS' => true
+    );
+    $pusher = new Pusher\Pusher(
+      'ac2b1faa8b9a8094de41',
+      'd078b9ab01a564abf58f',
+      '906718',
+      $options
+    );
+    $tableId = $newGETParams["tableId"];
+    $type = $newGETParams["type"];
+    $pusher->trigger($tableId, $type, $newGETParams);
+  }
+
+  function sendToNodeServer($newGETParams) {
+    $params = "?";
+    foreach ($newGETParams as $key => $value) {
+      $params .= $key."=".$value."&";
+    }
+    $url = "http://localhost:28887/sender".$params;
+    $output = file_get_contents($url);
+  }
+
+
+
 ?>
