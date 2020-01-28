7e60ec0c178b7c5cb6dbb1e7591713fd31d71386
Hajime Nagata <hajime.nagata@ncreate.net>
2020-01-28 11:25:36 +0900

    index.js 更新

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,7 +2,7 @@ $(function(){
     $(".myButton").click(function() {
         let who = $(".waku").val()
         console.log(who);
-        window.location.href = '/roomsearch.php?player='+who;
+        window.location.href = './roomsearch.php?player='+who;
     });
     $("body").keypress(function(e) {
         if (e.which === 13) {
