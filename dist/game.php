7f3828a87798bd32ebaba769cee0d5443105bd87
shouya <shouyatakayoshi@gmail.com>
2020-01-28 14:36:42 +0900

    aaa

--- dist/game.php
+++ dist/game.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0" />
 
     <title>othello</title>
+    <link rel="shortcut icon" href="./faviconstc.ico" />
     <link rel="stylesheet" href="css/style.css" />
 </head>
 
