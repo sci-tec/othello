174aa0e8da5ce86489f330b3202ac1ff49812ab9
Aren Tamae <tamaearen@gmail.com>
2020-01-29 11:25:39 +0900

    chat 変更

--- dist/chat.php
+++ dist/chat.php
@@ -11,10 +11,10 @@
         <p class="chat-str">チャット</p>
         <form method="post" action="chat.php">
             <div class="font">
-                <div class="left"> 名前　   　　　<input type="text" name="name"　size="15" placeholder="名前を入力"> </div>
-                <div class="right"> メッセージ    　<input type="text" name="message"　size="15" placeholder="メッセージ入力"></div>
+                <!-- <div class="left"> 名前　   　　　<input type="text" name="name"　size="15" placeholder="名前を入力"> </div> -->
+                <div class="right"> メッセージ    　<input type="text" name="message"　size="15" placeholder="メッセージ入力   Enterを押すと送信"></div>
             </div>
-            <button class="send" name="send" type="submit">送信</button>
+            <!-- <button class="send" name="send" type="submit">送信</button> -->
         </form>
         <!-- <p class="font">チャット履歴</p> -->
     </div>
@@ -29,7 +29,6 @@
                 $who = rand(1, 100) > 50 ? "me" : "you";
 
                 echo "<div class= profile><img src='../img/no_image.png' alt='icon' class = 'image'><div class='bubble'>".$message['name']."：".$message['message'].":".$message['time']."</div></div>";
-
                 // echo "<div class= profile>";
                 // echo '<img src="../img/no_image.png" alt="icon" class="image">';
                 // echo '<div class="bubble">'.$message['name']."：".$message['message'].":".$message['time']."</div>";
