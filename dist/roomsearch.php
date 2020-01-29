810aed35d0aea88d1ac0abade2e72255174c60fe
reo <iloveriido@icloud.com>
2020-01-29 15:48:47 +0900

    更新

--- dist/roomsearch.php
+++ dist/roomsearch.php
@@ -27,7 +27,7 @@
 <main>
     <div class="container">
         <div class="content">
-            <h1>player: <?php echo $_GET["player"]; ?></h1>
+            <h1>player: <?php echo $username; ?></h1>
             <div class="row">
                 <input type="text" name="room" placeholder="ルーム名" class="room">
                 <a href="#" id = "make" class="search">make</a> 
