08cece02462422ceb173a52521464226d4dea4cf
Hajime Nagata <hajime.nagata@ncreate.net>
2020-02-16 15:36:00 +0900

    結果登録＆セッション登録api作成

---
+++ dist/postResult.php
@@ -0,0 +1,16 @@
+<?php
+// $winnerid = preg_replace('/[^0-9a-zA-Z]/', '', $_POST['winnerid']);
+// $loserid = preg_replace('/[^0-9a-zA-Z]/', '', $_POST['loserid']);
+$winnerid = preg_replace('/[^0-9a-zA-Z]/', '', $_GET['winnerid']);
+$loserid = preg_replace('/[^0-9a-zA-Z]/', '', $_GET['loserid']);
+
+$sql_winner =  "update users SET win=win+1, count=count+1 where id = '".$winnerid."'";
+$sql_loser =  "update users SET lose=lose+1, count=count+1 where id = '".$loserid."'";
+
+require_once("./db.php");
+execSQL($sql_winner);
+execSQL($sql_loser);
+
+return "success";
+
+?>
