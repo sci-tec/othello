a9dbd450bcceb22b2563c17056d60bfac557a9c8
nakama <mingjiazhenyoutai@gmail.com>
2020-02-19 11:40:43 +0900

    駒の置ける場所を制御する処理を追加しました。

--- dist/js/othello2.js
+++ dist/js/othello2.js
@@ -365,22 +365,18 @@ const Controller = ((model, view) => {
 
   const addEvents = () => {
     $(document).on("click", ".cell", function() {
-      if (parseInt(data[1].currentColor) === data[0].myColor) {
+      if (parseInt(data[1].currentColor) === data[0].myColor &&
+         this.children[0] !== undefined &&
+         this.children[0].className === `hint${data[0].myColor}`) {
         var y = $(this).data("y");
         var x = $(this).data("x");
-        if (
-          data[1].cells[y][x].hint !== "" ||
-          data[1].cells[y][x].hint !== undefined
-        ) {
-          // console.log(data[1].cells[y][x].hint);
+          console.log(data[1].cells[y][x]);
           senderToPusher(x, y, data[1].currentColor);
-        }
         if (!data[1].hint && data[1].result === "") {
           senderToPusher(x, y, data[1].currentColor);
         }
       }
     });
-  };
 
   const postResult = (
     postData = { winnerid: session_userId, loserid: session_opponentId }
