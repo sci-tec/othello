6ceb030a55ef5c15d37787257ae556865115c95a
Hajime Nagata <hajime.nagata@ncreate.net>
2020-02-27 13:10:07 +0900

    登録件数制限、chatの通信機能修正

--- dist/addroom.php
+++ dist/addroom.php
@@ -1,7 +1,8 @@
 <?php
 $name = preg_replace('/[^0-9a-zA-Z]/', '', $_GET['name']);
 
-$sql_select =  "select * from rooms where name='".$name."'";
+// $sql_select =  "select * from rooms where name='".$name."'";
+$sql_select = "SET @d = ( SELECT id FROM rooms ORDER BY id DESC limit 28,1 ); DELETE FROM rooms WHERE id < @d; select * from rooms where name='".$name."';";
 $sql_insert = "insert INTO rooms (name,members) VALUES ('".$name."','')";
 
 require_once("./db.php");
